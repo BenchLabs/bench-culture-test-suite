c15bbd07536b2a134919eaa6a0b5666206bb168e
Blake Turner <blake.turner@bench.co>
2022-10-27 09:46:43 -0700

    tpd principles release version

--- src/principles.js
+++ src/principles.js
@@ -5,10 +5,10 @@ export default [
       "Value creation starts with a commitment to improve the status quo",
       "Building a thriving business will allow us to help small business owners at scale",
       "We find our best solutions by working backwards from customer needs",
-      "We win by increasing engagement while reducing toil",
-      "Saying “no” to great ideas means we’re working on even better ones",
       "Rapid iterations create short feedback loops that inform great solutions",
+      "The value we provide for our customers must be worth the effort we request from them",
       "If we can't measure our impact, we can't assess if we've created value",
+      "It\'s healthy to defer great ideas when our current work has greater impact"
     ]
   },
   {
@@ -19,7 +19,7 @@ export default [
       "Permission to express dissatisfaction + support to drive change = continuous improvement",
       "Feedback is most effective when those receiving it feel supported by those giving it",
       "For growth, periods of rest are as essential as periods of effort",
-      "The ultimate test of knowledge is the ability to teach it to others",
+      "Teaching multiplies the value of your knowledge"
     ]
   },
   {
@@ -29,9 +29,9 @@ export default [
       "Diverse perspectives and experiences contribute to better outcomes for everyone",
       "Effective organizations use a small set of objectives to coordinate work across teams",
       "Social bonds increase empathy, mutual respect, and our ability to do great things",
-      "For a team to succeed, they need adequate competency, context, and confidence",
+      "Context, capability, and trust are all essential for a successful autonomous team",
       "We are successful in setting timelines when they push us without burning us out",
-      "We break silos and build trust by making our goals, progress, and learnings readily available and easily digestible",
+      "We break silos and build trust by making our goals, progress, and learnings readily available and easily digestible"
     ]
   }
 ];
